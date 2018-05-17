dbdec5291e68aad379b10d8be91a11b53df8eabb
Adonalsium <sefrys@gmail.com>
2018-05-17 21:35:28 +0200

    Implement empty inherited methods

--- src/main/java/com/sefrys/TyperChecker/Spellchecker/SpellChecker.java
+++ src/main/java/com/sefrys/TyperChecker/Spellchecker/SpellChecker.java
@@ -1,8 +1,42 @@
 package com.sefrys.TyperChecker.Spellchecker;
 
+import com.sefrys.DataStructure.Hash.HashTable;
+
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by Ireneusz Zagan on 17.05.18, 21:34
  * Contact: sefrys@gmail.com
  */
-public class SpellChecker {
+public class SpellChecker extends HashTable implements SpellingChecker {
+    @Override
+    public Set<String> gatherSuggestions(String value) {
+        return null;
+    }
+
+    @Override
+    public List<String> adjacentLetterSwap(String value) {
+        return null;
+    }
+
+    @Override
+    public List<String> adjacentLetterInsert(String value) {
+        return null;
+    }
+
+    @Override
+    public List<String> letterDeletion(String value) {
+        return null;
+    }
+
+    @Override
+    public List<String> letterReplacement(String value) {
+        return null;
+    }
+
+    @Override
+    public List<String> wordSplitter(String value) {
+        return null;
+    }
 }
