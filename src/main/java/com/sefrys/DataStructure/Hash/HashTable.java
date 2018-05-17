7711dc2a61221710dcc8de928ba2342803744f9a
Adonalsium <sefrys@gmail.com>
2018-05-17 21:08:15 +0200

    Create class HashCollection

---
+++ src/main/java/com/sefrys/DataStructure/Hash/HashTable.java
@@ -0,0 +1,19 @@
+package com.sefrys.DataStructure.Hash;
+
+/**
+ * Created by Ireneusz Zagan on 17.05.18, 21:07
+ * Contact: sefrys@gmail.com
+ */
+public class HashTable extends HashCollection{
+    public void add(String value) {
+
+    }
+
+    public void remove(String value) {
+
+    }
+
+    public boolean lookup(String value) {
+        return false;
+    }
+}
