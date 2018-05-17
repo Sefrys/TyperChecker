bf1f8d502c60670aab5e73c451caf9a460ae7569
Adonalsium <sefrys@gmail.com>
2018-05-17 20:55:09 +0200

    Create base class fields

--- src/main/java/com/sefrys/DataStructure/Hash/HashCollection.java
+++ src/main/java/com/sefrys/DataStructure/Hash/HashCollection.java
@@ -1,8 +1,15 @@
 package com.sefrys.DataStructure.Hash;
 
+import java.util.LinkedList;
+
 /**
  * Created by Ireneusz Zagan on 17.05.18, 20:53
  * Contact: sefrys@gmail.com
  */
 public abstract class HashCollection {
+
+    protected LinkedList<String>[] table;
+    protected final int INIT_SIZE = 128;
+    protected final int LINKED_LIST_SIZE = 128;
+    protected int size;
 }
